f3b616d36af521e61d4088c08fea10bd537c9265
AkankshaKorasikha <118505298+AkankshaKorasikha@users.noreply.github.com>
2022-12-08 19:10:57 +0530

    Add files via upload

---
+++ pythonproject.py
@@ -0,0 +1,190 @@
+a='''
+                                                 This new old house
+We bought an old house, my boyfriend and I. He’s in charge of the “new” construction – converting the kitchen in to the 
+master bedroom for instance, while I’m on wallpaper removalduty. The previous owner papered EVERY wall and CEILING! Removing 
+it is brutal, but oddly satisfying. The best feeling is getting a long peel, similar to your skin when you’re peeling from a 
+sunburn. I don’t know about you but I kinda make a game of peeling, on the hunt for the longest piece before it rips.Under a 
+corner section of paper in every room is a person’s name and a date. Curiosity got the best of me one night when I Googled one 
+of the names anddiscovered the person was actually a missing person, the missing date matching the date under the wallpaper! 
+The next day, I made a list of all the names and dates. Sure enough each name was for a missing person with dates to match. 
+We notified the police who naturally sent out the crime scene team. I overhead one tech say “yup, it’s human.” Human? What’s 
+human? “Ma’am, where is the material you removed from the walls already?This isn’t wallpaper you were removing.”
+'''
+b='''
+                                 I hate it when my brother Charlie has to go away         
+I hate it when my brother Charlie has to go away. My parents constantly try to explain to me how sick he is. That I am lucky 
+for having a brain where all the chemicals flow properly to their destinations like undammed rivers. When I complain about 
+how bored I am without a little brother to play with, they try to make me feel bad by pointing out that his boredom likely  
+far surpasses mine, considering his confine to a dark room in an institution. I always beg for them to give him one last 
+chance. Of course, they did at first. Charlie has been back home several times, each shorter in duration than the last. 
+Every time without fail, it all starts again. The neighbourhood cats with gouged out eyes showing up in his toy chest, my 
+dad’s razors found dropped on the baby slide in the park across the street, mom’s vitamins replaced by bits of dishwasher 
+tablets. My parents are hesitant now, using “last chances” sparingly. They say his disorder makes him charming, makes it 
+easy for him to fake normalcy, and to trick the doctors who care for him into thinking he is ready for rehabilitation. That 
+I will just have to put up with my boredom if it means staying safe from him. I hate it when Charlie has to go away. It 
+makes me have to pretend to be good until he is back.
+'''
+c='''
+                                            Guardians
+He awoke to the huge, insect like creatures looming over his bed and screamed his lungs out. They hastily left the room and 
+he stayed up all night, shaking and wondering if it had been a dream.The next morning, there was a tap on the door. Gathering
+his courage, he opened it to see one of them gently place a plate filled with fried breakfast on the floor, then retreat to
+a safe distance. Bewildered, he accepted the gift. The creatures chittered excitedly.This happened every day for weeks. At 
+first he was worried they were fattening him up, but after a particularly greasy breakfast left him clutching his chest from 
+heartburn, they were replaced with fresh fruit. As well as cooking, they poured hot steamy baths for him and even tucked him
+in when he went to bed. It was bizarre.One night, he awoke to gunshots and screaming. He raced downstairs to find a 
+decapitated burglar being devoured by the insects. He was sickened,but disposed of the remains as best he could. He knew 
+they had just been protecting him.One morning the creatures wouldn’t let him leave his room. He lay down, confused but 
+trusting as they ushered him back into bed. Whatever their motives, they weren’t going to hurt him.Hours later a burning 
+pain spread throughout his body. It felt like his stomach was filled with razor wire. The insects chittered as he spasmed 
+and moaned. It was only when he felt a terrible squirming feeling beneath his skin that he realised the insects hadn’t been 
+protecting him. They had been protecting their young.
+'''
+a1=''' 
+                                        The Boy Who Cried Wolf
+There was once a shepherd boy who liked to play tricks. One day, while he was watching over the herd, the boy decided to play
+a trick and cried “wolf! wolf!”. The people who heard rushedover to help him. But they were disappointed when they saw that 
+there was no wolf and the boywas laughing at them. The next day, he did it again and people rushed to his aid only to be 
+disappointed once again. On the third day, the boy saw a wolf devouring one of his sheep and cried for help. But the people 
+who heard him thought this is just another of the boy’s pranks so no one came to help him. That day, the boy lost some of 
+his sheep to the wolf.
+
+Moral of the story:
+If you always lie and cheat on other people, there will come a time when no one will believe you anymore.
+'''
+b1=''' 
+                                           The Ugly Duckling
+Most of us have probably heard of this story as this is one of the most popular fairy tales in the world. The story revolves 
+around a duckling who from the moment of his birth has always felt different from his siblings. He was always picked on 
+because he didn’t look like the rest of them. One day, he had enough and ran away from the pond he grew up in. He wandered 
+near and far looking for a family who would accept him. Months passed and seasons changed but everywhere he went, nobody 
+wanted him because he was such an ugly duck. Then one day, he came upon a family of swans. Upon looking at them, he realized 
+that during the months he spent looking for a family to call his own, he had grown into a beautiful swan. Now he finally 
+understood why he never looked like the rest of his siblings because he isn’t a duck but a swan.
+
+Moral of the story:
+
+We shouldn’t be too quick to judge others based on their physical appearance. Just because someone doesn’t fit societal 
+definitions of beauty doesn’t mean they’re ugly. Each of us is beautiful in our own unique way and it’s time we accept and 
+celebrate that individuality.                           
+ '''
+c1=''' 
+                                          The Lion and the Poor Slave
+There was once a slave who was treated cruelly by his master. One day, he couldn’t take it anymore and ran away to the forest
+to escape. There he chanced upon a lion who couldn’t walk because of the thorn in its paw. Although he’s scared, the slave 
+mustered his courage and took out the thorn in the lion’s paw. When the lion was finally free of the thorn, he ran into the 
+forest and didn’t harm the slave. Sometime later, the slave was caught by his master along with some animals in the forest. 
+The master then ordered the slave to be thrown into the lion’s den. When the slave saw the lion, he recognized it as the same 
+lion he helped in the forest. The slave was able to escape the den unharmed and he freed all the other animals.
+
+Moral of the story:
+
+The good you did will always have a way of coming back to you. So do good deeds and be kind to others and the world will be 
+kind to you.
+'''
+a2='''
+                                        A SCOTTISH RACE IN THE DARK
+You don’t realize how unfit you are til you’ve participated in a night scavenger hunt with a Scottish mountaineering club. 
+My flatmate talked Nathan and I into it, so off we went, equipped with flashlight aka “torch,” and a friend who could 
+navigate.As I huffed and puffed up the heathery hills, I couldn’t help but wonder what I’d gotten myself into. We walked 
+across mountain tops with nothing but sheep and waded through muck and mire.
+At the end, a roaring bonfire greeted us, and everyone toasted a job well-done, even the mostly American team which came in 
+last.
+'''
+b2=''' 
+                                          ALMOST A PRAGUE PRISON
+Don’t ride the trams in Prague without a ticket. Sounds like common sense, but my friend and I thought one quick stop 
+wouldn’t hurt.
+Oh, we were noticed.
+Two men asked for our tickets. Having none, we were escorted off the tram at the next stop. But we weren’t set free. 
+The men demanded we pay the hefty fine for riding without a ticket. They wouldn’t listen to our pleas and marched us to an 
+ATM. They said “either you pay or we go to Czech police.”90 dollars poorer. I’ll never again try to ride a tram for free.
+'''
+c2='''
+                                                  THE DAYTONA 500
+Daytona Beach. 2004. My dance team was invited to be part of the famous NASCAR race: in a patriotic opening number.
+We were only a small number in a massive crowd. I had to arrive at 3am and practice for two straight days in the rain, but 
+there were a few standout moments during the event.
+President George W. Bush waved (at me, I swear) as he exited his helicopter.Right before performing her big number “ROCK in 
+the USA”, I saw LeeAnn Rimes mouth the words, “oh shit.” All in all, a good day to be on live TV!
+'''
+a3='''
+                                                 Blue Monkeys
+Cautiously, the girl stepped out of her cosy bed because she could hear tapping on the windowwhich gave her fright. She 
+gingerly and silently crept over to the window and the icy, cold, wooden floorboards creaked behind her. With lots of 
+courage she slowly opened the curtains but to her surprise she couldn’t see anything. She looked again into the dark night 
+to check but she was sure that nothing was there. All of a sudden, the girl could see two bright lights drawing closer and 
+closer out of the darkness. The girl was shocked! What could they be?
+'''
+b3='''
+                                       Splodgy Bananas says
+Silently, she crawled out of bed. There it was again, tap, tap, tap. She shivered to the window out of fright. She drew the 
+curtains open, but all that was there were the stars of the night sky. Suddenly, two bright yellow lights were coming closer
+and closer.
+Then a face appeared. It had sharp teeth but she realized it was only her best friend Jack. She begged for him to come in. 
+Jack dashed in. She got a closer look at his fangs and realized he was a vampire. He was tempted to bite Emily. What would 
+he do?
+'''
+c3='''
+                                                Spooky spy girls!
+ Breathlessly panting, the girls raced towards the dark, spooky forest as the wind was blowing ferociously. The ground was as 
+hard as rock and the tree branches crouched over their heads like a terrifying monster. Suddenly, a rustle of crunchy, red 
+leaves started to blow madly. 
+Without a warning, a shadowy figure slyly came out of the bushes and blocked Lizzie’s way. The shadowy figure had an axe and
+cut a tree down. CRACK, CRACK, CRACK! The humungous tree started falling. Lizzie started to call her friend but she carried 
+on running into the cold,darkness of the forest…
+ '''
+a4='''
+                                         Alarm Clocks
+I always set my alarm clock before I go to sleep, and I always set the alarm clock on my phone as a backup.Even though I 
+set two alarms every morning, I wake up before them. But I still set those alarms, just in case I sleep late and need them.
+It’s like circus acrobats and trapeze artists who use nets. If they are good, they don’t need or even want the net, but they
+have it there anyway.
+So, I set the alarms, go to bed, and I dream of being a circus acrobat.Without a goddamned net
+'''
+b4=''' 
+                                       Up in the Air
+The circus came to town the day I left. I took Lisa to the show, knowing she still loved the clowns. First act was juggling. 
+Lisa watched the balls arced and swooped, then exploded into the air like multicolored popcorn. Their movement was a series 
+of surprises so fluid that thejuggler seemed more audience member than facilitator. But there was a moment when his hands 
+were empty, and he could only wait as his balls flew around him. I saw his relief as each ball returned to his open arms. 
+But I left that night and didn’t look back. 
+'''
+c4=''' 
+                                     Camelot's Backyard
+We were only warriors. Eight-year-olds forging new frontiers, watching Rome fall. We were brothers in arms, bandits, 
+thieves - but you named me damsel. We shaped palace walls from snow, tore through forbidden forest with plastic machetes. 
+We ate poisoned berries like our stomachs were made of Excalibur, melted down and recast. We could never die. You, with your 
+cardboard helmet and shield, were noblest of all. You gutted more dragons than the rest of us combined. But I, with my cheap 
+silk cape and tissue paper crown, was stronger. I was neverwarrior, never damsel. I was made to rule.
+'''
+
+import random
+horror_list=[a,b,c]
+thriller_list=[a3,b3,c3]
+moral_list=[a1,b1,c1]
+adventure_list=[a2,b2,c2]
+fantasy_list=[a4,b4,c4]
+print('1.Thriller Stories')
+print('2.Horror Stories')
+print('3.Moral Stories')
+print('4.Adventure stories')
+print('5.Fantasy Stories')
+print()
+while True:
+    a = int(input('         Select the genre you like: '))
+    if 1<=a<=5:
+        break
+    else:
+        print('invalid')
+        continue
+if a==1:
+    print(random.choice(thriller_list))
+elif a==2:
+    print(random.choice(horror_list))
+elif a==3:
+    print(random.choice(moral_list))
+elif a==4:
+    print(random.choice(adventure_list))
+else:
+    print(random.choice(fantasy_list))
